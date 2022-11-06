1a593f8a9b2d6170368fa2b413c8a0c68e41eea7
Tomoyuki <season2864@gmail.com>
2022-11-07 00:47:17 +0900

    change: delete option processing

--- cli.js
+++ cli.js
@@ -1,21 +1,20 @@
 import { Command } from "commander";
-const program = new Command();
 import fetch from "node-fetch";
 import jsdom from "jsdom";
 import file from "./util/file.js";
-import prompts from "prompts";
 import Table from "cli-table3";
 const { JSDOM } = jsdom;
+const program = new Command();
 
 program
-  .version("0.0.2")
+  .version("0.0.1")
   .option(
     "-a, --add <url>",
     "Add notification target ex: https://github.com/<username>/<repository>",
     null
   )
   .option("-l, --list", "List the target URLs.")
-  .option("-d, --del", "Delete the target URL.")
+  .option("-d, --del <no>", "Delete the target URL.", null)
   .parse(process.argv);
 
 const options = program.opts();
@@ -47,24 +46,6 @@ if (options.add) {
 }
 
 if (options.list) {
-  let table = new Table({ head: ["URL", "Version"] });
-
-  const fileCharacters = file.readFileCharacter();
-  const perLineArray = fileCharacters.split("\n");
-
-  for (const perLine of perLineArray) {
-    const temp = perLine.split(" ");
-    const url = temp[0];
-    const localVersion = temp[1];
-    if (!(url == "" || localVersion == undefined))
-      table.push([url, localVersion]);
-  }
-
-  console.log(table.toString());
-  process.exit(0);
-}
-
-if (options.del) {
   let table = new Table({ head: ["#", "URL", "Version"] });
   let index = 1;
 
@@ -80,34 +61,36 @@ if (options.del) {
   }
 
   console.log(table.toString());
+  process.exit(0);
+}
 
-  (async () => {
-    let deleteNo;
-    const response = await prompts({
-      type: "number",
-      name: "value",
-      message: "Which URLs do you want to delete? Please specify with #.",
-      validate: (value) => (value > 0 ? (deleteNo = value) : false),
-    });
+if (options.del) {
+  let table = new Table({ head: ["#", "URL", "Version"] });
+  let urls = [];
 
-    if (response) {
-      const fileCharacters = file.readFileCharacter();
-      const perLineArray = fileCharacters.split("\n");
-      file.deleteFile();
-      file.createFile();
-      let index = 1;
-      for (const perLine of perLineArray) {
-        const temp = perLine.split(" ");
-        const url = temp[0];
-        const localVersion = temp[1];
-        if (deleteNo == index++) {
-          continue;
-        }
-        if (!(url == "" || localVersion == undefined)) {
-          file.addNotificationTarget(url, localVersion);
-        }
-      }
+  const fileCharacters = file.readFileCharacter();
+  const perLineArray = fileCharacters.split("\n");
+  file.deleteFile();
+  file.createFile();
+  let index = 1;
+  for (const perLine of perLineArray) {
+    const temp = perLine.split(" ");
+    const url = temp[0];
+    const localVersion = temp[1];
+    if (options.del == index++) {
+      continue;
     }
-  })();
-  // process.exit(0);
+    if (!(url == "" || localVersion == undefined)) {
+      file.addNotificationTarget(url, localVersion);
+      table.push([index++, url, localVersion]);
+      urls.push(url);
+    }
+  }
+  console.log("URL has been removed.");
+  if (urls.length != 0) {
+    console.log("The remaining URLs are listed below.");
+    console.log(table.toString());
+  }
+
+  process.exit(0);
 }
